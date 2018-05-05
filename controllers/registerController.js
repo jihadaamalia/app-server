39009666b271f4192d6780e057fad0bd923bfccc
jihadaamalia <jihadaamalia@yahoo.com>
2018-05-06 00:54:04 +0700

    only update on user_profile

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -5,77 +5,32 @@ require('dotenv').config();
 
 module.exports.createUserProf = function (req, res) { //step 1 of user registration data
     var userData = req.body;
-    var checkDataSql = "SELECT id FROM `user_profile` WHERE `username`= '" + res.locals.username + "'";
-    db.query(checkDataSql, function(err, result){
+    var updateUserProfile = "UPDATE `user_profile` SET `name` = '" + userData.name + "', `user_dob` = '" + userData.user_dob + "', `sex` = '" + userData.sex + "', `photo` = '" + userData.photo + "', `city` = '" + userData.city + "', updated_at = CURRENT_TIMESTAMP() WHERE `username_id` = '" + res.locals.username_id + "'";
+    db.query(updateUserProfile, function(err, result){
         if(err) {
             res.json({
                 status: 500,
                 error: true,
                 error_msg: {
-                    title: 'Failed to fetch data',
+                    title: 'Failed to update data',
                     detail: err
                 },
                 response: ''
             });
             res.end();
-        } else if (result[0]) { //if data has created
-            //TODO: USE IF EXIST INSTEAD OF 3 DIFF QUERY
-            var updateUserProfile = "UPDATE `user_profile` SET `name` = '" + userData.name + "', `user_dob` = '" + userData.user_dob + "', `sex` = '" + userData.sex + "', `photo` = '" + userData.photo + "', `city` = '" + userData.city + "', updated_at = CURRENT_TIMESTAMP() WHERE `username` = '" + res.locals.username + "'";
-            db.query(updateUserProfile, function(err, result){
-                if(err) {
-                    res.json({
-                        status: 500,
-                        error: true,
-                        error_msg: {
-                            title: 'Failed to update data',
-                            detail: err
-                        },
-                        response: ''
-                    });
-                    res.end();
-                } else {
-                    res.json({
-                        status: 200,
-                        error: false,
-                        error_msg: {
-                            title: '',
-                            detail: ''
-                        },
-                        response: 'User profile updated!'
-                    });
-                    res.end();
-                }
+        } else {
+            res.json({
+                status: 200,
+                error: false,
+                error_msg: {
+                    title: '',
+                    detail: ''
+                },
+                response: 'User profile updated!'
             });
-        } else { //create new data
-            var createUserProfile = "INSERT INTO `user_profile` (`name`, `user_dob`, `sex`, `username`, `photo`, `city`, `added_at`)  VALUES ('" + userData.name + "', '" + userData.user_dob + "', '" + userData.sex + "', '" + res.locals.username + "', '" + userData.photo + "', '" + userData.city + "', CURRENT_TIMESTAMP())";
-            db.query(createUserProfile, function(err, result){
-                if(err) {
-                    res.json({
-                        status: 500,
-                        error: true,
-                        error_msg: {
-                            title: 'Failed to insert data',
-                            detail: err
-                        },
-                        response: ''
-                    });
-                    res.end();
-                } else {
-                    res.json({
-                        status: 200,
-                        error: false,
-                        error_msg: {
-                            title: '',
-                            detail: ''
-                        },
-                        response: 'User profile created!'
-                    });
-                    res.end();
-                }
-            });
+            res.end();
         }
     });
-
 };
 
 module.exports.createPet = function (req, res) {
