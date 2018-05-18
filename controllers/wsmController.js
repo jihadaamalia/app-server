23013c06a67ad95e868dc9cc98c46abcd6e32004
jihadaamalia <jihadaamalia@yahoo.com>
2018-05-18 10:32:58 +0700

    calculate age, size

--- controllers/wsmController.js
+++ controllers/wsmController.js
@@ -1,27 +1,27 @@
 module.exports.calculate = function (alternative, resource) {
 
     var self = this;
-
-    self.weight = [
+    self.optimalAge = 1;
+    self.criteria = [
+        {
+            title : 'age',
+            weight : 0.33,
+            score : [0.25,0.5,0.75,1]
+        },
         {
             title : 'size',
-            weight : 0.33,
-            score : [0,1]
+            weight : 0.27,
+            score : [0.25,0.5,0.75,1]
         },
         {
             title : 'health',
-            weight : 0.27,
-            score : [0,1]
+            weight : 0.2,
+            score : [0.25,0.5,1]
         },
         {
             title : 'breed',
-            weight : 0.2,
-            score : [0,1]
-        },
-        {
-            title : 'age',
             weight : 0.13,
-            score : [0,1]
+            score : [0.25,0.5,0.75,1]
         },
         {
             title : 'city',
@@ -66,32 +66,95 @@ module.exports.calculate = function (alternative, resource) {
         }
     })();
 
+    //define pet sex
+    if (resource.pet_sex == 'f') {
+        //console.log(alternative[0])
+        self.pet_f = resource;
+        self.pet_m = alternative;
+        console.log(alternative.length)
+
+        for (var i in self.pet_m) {
+            alternative[i].scores = {
+                age: 0,
+                size: 0,
+                health: 0,
+                breed: 0,
+                city: 0
+            };
+
+            self.pet_m[i].age = getAge(self.pet_m[i].pet_dob);
+            self.pet_f.age = getAge(self.pet_f.pet_dob)
+
+            if (self.pet_f.age >= self.optimalAge && self.pet_m[i].age >= self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[3];
+            } else if (self.pet_f.age >= self.optimalAge && self.pet_m[i].age < self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[2];
+            } else if (self.pet_f.age < self.optimalAge && self.pet_m[i].age >= self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[1];
+            } else if(self.pet_f.age < self.optimalAge && self.pet_m[i].age >= self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[0];
+            }
+
+            if (self.pet_f.size == self.pet_m[i].size) {
+                alternative[i].scores.size = self.criteria[1].score[3];
+            } else if (self.pet_f.size == 'large' && self.pet_m[i].size == 'medium' ||
+                       self.pet_f.size == 'medium' && self.pet_m[i].size == 'small') {
+                alternative[i].scores.size = self.criteria[1].score[2];
+            } else if (self.pet_f.size == 'medium' && self.pet_m[i].size == 'large' ||
+                       self.pet_f.size == 'small' && self.pet_m[i].size == 'medium') {
+                alternative[i].scores.size = self.criteria[1].score[1];
+            } else {
+                alternative[i].scores.size = self.criteria[1].score[0];
+            }
+
+            //console.log(alternative[i].scores.size, ' ',alternative[i].scores.age,' ', i)
+        }
+    } else {
+        self.pet_m = resource;
+        self.pet_f = alternative;
+
+        for (var i in self.pet_m) {
+            alternative[i].scores = {
+                age: 0,
+                size: 0,
+                health: 0,
+                breed: 0,
+                city: 0
+            };
+
+            self.pet_m.age = getAge(self.pet_m.pet_dob);
+            self.pet_f[i].age = getAge(self.pet_f[i].pet_dob)
+
+            if (self.pet_f[i].age >= self.optimalAge && self.pet_m.age >= self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[3];
+            } else if (self.pet_f[i].age >= self.optimalAge && self.pet_m.age < self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[2];
+            } else if (self.pet_f[i].age < self.optimalAge && self.pet_m.age >= self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[1];
+            } else if(self.pet_f[i].age < self.optimalAge && self.pet_m.age >= self.optimalAge) {
+                alternative[i].scores.age = self.criteria[0].score[0];
+            }
+
+            if (self.pet_f[i].size == self.pet_m.size) {
+                alternative[i].scores.size = self.criteria[1].score[3];
+            } else if (self.pet_f[i].size == 'large' && self.pet_m.size == 'medium' ||
+                self.pet_f[i].size == 'medium' && self.pet_m.size == 'small') {
+                alternative[i].scores.size = self.criteria[1].score[2];
+            } else if (self.pet_f[i].size == 'medium' && self.pet_m.size == 'large' ||
+                self.pet_f[i].size == 'small' && self.pet_m.size == 'medium') {
+                alternative[i].scores.size = self.criteria[1].score[1];
+            } else {
+                alternative[i].scores.size = self.criteria[1].score[0];
+            }
+        }
+    }
+
     for (var i in alternative) {
         alternative[i].matched_status = {
             score : 0
-        };
-
-        if (resource.size == alternative[i].size) {
-            alternative[i].matched_status.score = alternative[i].matched_status.score + (self.weight[0].weight * self.weight[0].score[1]);
         }
 
-        if (alternative[i].vaccines.length > 0) {
-            alternative[i].matched_status.score = alternative[i].matched_status.score + (self.weight[1].weight * self.weight[1].score[1]);
-        }
-
-        if (resource.breed_pref == alternative[i].breed) {
-            alternative[i].matched_status.score = alternative[i].matched_status.score + (self.weight[2].weight * self.weight[2].score[1]);
-        }
-
-        alternative[i].age = getAge(alternative[i].pet_dob)
-
-        if (resource.age_min <= alternative[i].age && resource.age_max <= alternative[i].age) {
-            alternative[i].matched_status.score = alternative[i].matched_status.score + (self.weight[3].weight * self.weight[3].score[1]);
-        }
-
-        if (resource.city_pref == alternative[i].city_id) {
-            alternative[i].matched_status.score = alternative[i].matched_status.score + (self.weight[4].weight * self.weight[4].score[1]);
-        }
+        alternative[i].matched_status.score = (self.criteria[0].weight * alternative[i].scores.age) + (self.criteria[1].weight * alternative[i].scores.size) + (self.criteria[2].weight * alternative[i].scores.health) + (self.criteria[3].weight * alternative[i].scores.breed) + (self.criteria[4].weight * alternative[i].scores.city);
     }
 
     //sort by score
